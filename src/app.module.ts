0bfbaac0c6e4cd2db91922fd057bcb3d412a17f4
Edilson R Lima <edilson.lima@meta.com.br>
2023-12-22 18:43:13 -0400

    Refactor app module to remove unused imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { OmdbModule } from './modules/omdb/omdb.module';
 
 @Module({
   imports: [OmdbModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
