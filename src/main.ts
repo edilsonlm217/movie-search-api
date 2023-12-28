b424b3ff1635942e23c8b3b93638df06ab15f3d6
Edilson R Lima <edilson.lima@meta.com.br>
2023-12-28 09:07:42 -0400

    Enabling cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+  app.enableCors();
   await app.listen(port);
 }
 bootstrap();
